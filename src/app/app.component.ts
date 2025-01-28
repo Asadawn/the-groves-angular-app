15322c8c61cdc191b6739060a2ac2297b6459042
Asadawn <asadawn3@gmail.com>
2025-01-27 19:44:31 -0500

    updates some file and add assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { ManubarComponent } from "./manubar/manubar.component";
+import { HomeComponent } from "./home/home.component";
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, ManubarComponent, HomeComponent, HeroSectionComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-angular-app';
+
+//  constructor(private router: Router){
+
+//  }
+//   routerFuncion(route: string){
+//    this.router.navigate([`/${route}`])
+//   }
+  
 }
